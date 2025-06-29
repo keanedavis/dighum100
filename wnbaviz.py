8c75e37070c4eeb00b6c28e1d1b6c88f166202e1
keanedavis <keaned@berkeley.edu>
2025-06-29 14:43:14 -0700

    Add files via upload

--- wnbaviz.py
+++ wnbaviz.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
+import plotly.express as px
 
 # Set Streamlit page configuration
 st.set_page_config(layout="wide", page_title="WNBA Attendance Dashboard", page_icon="🏀")
